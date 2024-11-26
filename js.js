2649f304944d174ad20e72521384bb8c205016a0
I-Dont-Know-What-Iam-Doing <165470919+I-Dont-Know-What-Iam-Doing@users.noreply.github.com>
2024-11-26 10:47:40 +0800

    qwerrewq

--- js.js
+++ js.js
@@ -48,14 +48,3 @@ function closeNav() {
 // - side nav
 
 
-// Login
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-// - Login
